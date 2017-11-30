5b04294ce644006751979a2b02d5882834e012f5
login123one <1973133742@qq.com>
2017-11-30 11:09:19 +0800

    666

--- src/com/git/Git.java
+++ src/com/git/Git.java
@@ -5,6 +5,8 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("This is my first gitProgramm");
 		System.out.println("This is my first gitProgramm");
+		
+		System.out.println("六不六");
 	}
 
 }
