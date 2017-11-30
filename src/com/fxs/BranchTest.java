83dd32494fd9dd489720a941a9d4719c9a9fde7a
login123one <1973133742@qq.com>
2017-11-30 14:51:32 +0800

    m

--- src/com/fxs/BranchTest.java
+++
@@ -1,7 +0,0 @@
-package com.fxs;
-
-public class BranchTest {
-	public static void main(String[] args) {
-		System.out.println("合并分支");
-	}
-}
--- src/com/git/Gitt2.java
+++
@@ -1,9 +0,0 @@
-package com.git;
-
-public class Gitt2 {
-
-	public static void main(String[] args) {
-		System.out.println("This is my second gitProgramm");
-	}
-
-}
